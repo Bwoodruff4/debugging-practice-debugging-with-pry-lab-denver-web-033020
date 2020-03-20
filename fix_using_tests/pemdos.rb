8174c7645544c304eeb1fe266c78d5b1fe70fd02
Blake <blakewoodruff4@gmail.com>
2020-03-20 18:58:17 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,10 @@
 def snake_it_up(string)
   if string[0] == "s"
-  #binding.pry
-  ten_s=10.times do "s" 
-  #ten_s+string
-  binding.pry
+    #binding.pry
+     
+    #ten_s+string
+    binding.pry
+  else
+    string
   end
 end

1d9b61e7e157e9b7f01e22b6a0df53ae58d04efe
Blake <blakewoodruff4@gmail.com>
2020-03-20 18:44:52 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,9 @@
 def snake_it_up(string)
   if string[0] == "s"
-    #binding.pry
-  (10 * "s") + string.to_s
+  #binding.pry
+  new_string =(10 * "s") + string.to_s
   else
-  string
+  new_string=string.to_s
   end
+  return new_string
 end

4751114186c33c59ffefcacfbac160d80052e6e5
Blake <blakewoodruff4@gmail.com>
2020-03-20 18:49:29 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,9 @@
 def snake_it_up(string)
   if string[0] == "s"
   #binding.pry
-  new_string =(10 * "s") + string
+  (10 * "s") + string
   else
-  new_string=string
+  string
   end
-  return new_string
+  
 end

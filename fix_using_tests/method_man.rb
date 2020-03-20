6278c57636b8019f1baef438709eb7e5947fc330
Blake <blakewoodruff4@gmail.com>
2020-03-20 18:34:51 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,10 +1,10 @@
 require 'pry'
 
 def start_game(player1, player2)
-  binding.pry
-  return "Hello #{player1} & #{playar2}"
+  #binding.pry
+  message ="Hello #{player1} & #{playar2}"
 end
 
 def play_game(player2)
-  return "#{player1} is better than #{player2}"
+  message= "#{player1} is better than #{player2}"
 end
